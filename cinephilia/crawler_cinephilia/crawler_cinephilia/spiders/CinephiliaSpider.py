daaa559521b22c606df4c14d19b48e88693cabce
Rafael-Cheng <rafaelcheng13@gmail.com>
2017-11-02 19:45:56 +0800

    add cinephilia spider

---
+++ cinephilia/crawler_cinephilia/crawler_cinephilia/spiders/CinephiliaSpider.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+
+import sys
+import scrapy
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+class CinephiliaSpider(scrapy.Spider):
+    name = "cine"
+    pre_url = "http://cinephilia.net/page/"
+    page = 1
+
+    def start_requests(self):
+        url = self.pre_url + str(self.page)
+        self.page += 1
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        articles = response.xpath('//div[@class="posts border ajaxify-pagination"]/article')
+        for article in articles:
+            url = article.xpath('.//figure[@class="post-gallery "]/a/@href').extract_first()
+            yield scrapy.Request(url=url, callback=self.parsePage)
+        url = self.pre_url + str(self.page)
+        self.page += 1   
+        yield scrapy.Request(url=url, callback=self.parse)
+
+    def parsePage(self, response):
+        with open('files/' + str(hash(response.url)), 'w') as f:
+            f.write(response.text)
